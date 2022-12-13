a352ae6a4c753a162d999e747e3f279f0bed155c
saifion33 <mxofficialplayer@gmail.com>
2022-12-13 11:20:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="App text-white bg-slate-800 text-center p-8 text-2xl">
+      This App is Running with Tailwindcss
+    </div>
+  )
+}
+
+export default App
